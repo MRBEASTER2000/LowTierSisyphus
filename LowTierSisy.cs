e480ae4c25f9e366e7bd360652c3f36da60427d2
ImNotSimon <129795437+ImNotSimon@users.noreply.github.com>
2024-04-21 16:50:05 +0100

    Update for AI-less update
    changed time where subtitle starts & added two or three new subtitles

--- LowTierSisy.cs
+++ LowTierSisy.cs
@@ -58,31 +58,33 @@ namespace LowTierSisy
                     {
                         bool replaced = false;
                         var subtitles = new List<SubtitledAudioSource.SubtitleDataLine>();
-                        if (source.clip.GetName() == "sp_intro")
+                        if (source.clip.name == "sp_intro")
                         {
                             Debug.Log("Replacing sissypuss intro");
-                            source.clip = LowTierSisyBundle.LoadAsset<AudioClip>("lowtiersisy.mp3");
+                            source.clip = LowTierSisyBundle.LoadAsset<AudioClip>("lowtiersisy.wav");
                             replaced = true;
 
                             subtitles.Add(MakeLine("You are a worthless", 0f));
-                            subtitles.Add(MakeLine("BITCH ASS machine.", 1.5f));
-                            subtitles.Add(MakeLine("Your life LITERALLY is as valuable as a", 3.5f));
-                            subtitles.Add(MakeLine("summer ant.", 5.5f));
-                            subtitles.Add(MakeLine("I'm just gonna stomp you,", 7.6f));
-                            subtitles.Add(MakeLine("and you're gonna keep coming back.", 8.8f));
-                            subtitles.Add(MakeLine("I'mma seal up all my cracks,", 10.4f));
-                            subtitles.Add(MakeLine("you're gonna keep coming back.", 12.1f));
-                            subtitles.Add(MakeLine("Why?", 13.7f));
-                            subtitles.Add(MakeLine("'Cause YOU smellin' the BLOOD,", 14.6f));
-                            subtitles.Add(MakeLine("you worthless", 17.1f));
-                            subtitles.Add(MakeLine("BITCH ASS machine.", 18.0f));
-                            subtitles.Add(MakeLine("You're gonna stay on my dick until you DIE.", 20.2f));
-                            subtitles.Add(MakeLine("You serve no purpose in life.", 23.3f));
-                            subtitles.Add(MakeLine("Your purpose in life is to be on my stream sucking on my dick daily.", 25.6f));
-                            subtitles.Add(MakeLine("Your purpose in life is to be in that chat BLOWING a dick daily,", 30.5f));
-                            subtitles.Add(MakeLine("your life is nothing, you serve ZERO purpose!", 34.1f));
-                            subtitles.Add(MakeLine("YOU SHOULD KILL YOURSELF", 36.8f));
-                            subtitles.Add(MakeLine("NOW!", 38.1f));
+                            subtitles.Add(MakeLine("BITCH ASS machine.", 1.1f));
+                            subtitles.Add(MakeLine("Your life LITERALLY is as valuable as a summer's ant.", 2.26f));
+                            subtitles.Add(MakeLine("I'm just gonna stomp you,", 5.86f));
+                            subtitles.Add(MakeLine("and you're gonna keep coming back.", 7.36f));
+                            subtitles.Add(MakeLine("I'mma seal up all my cracks,", 9.77f));
+                            subtitles.Add(MakeLine("you're gonna keep coming back.", 11.48f));
+                            subtitles.Add(MakeLine("Why?", 13.28f));
+                            subtitles.Add(MakeLine("'Cause YOU smellin' the BLOOD,", 13.87f));
+                            subtitles.Add(MakeLine("you worthless", 15.5f));
+                            subtitles.Add(MakeLine("BITCH ASS machine.", 16.5f));
+                            subtitles.Add(MakeLine("You're gonna stay on my dick until you DIE.", 17.78f));
+                            subtitles.Add(MakeLine("You serve no purpose in life.", 20.54f));
+                            subtitles.Add(MakeLine("Your purpose in life is to be on my stream", 22.021f));
+                            subtitles.Add(MakeLine("SUCKING on my dick daily.", 24.34f));
+                            subtitles.Add(MakeLine("Your purpose in life is to be in that chat", 26.605f));
+                            subtitles.Add(MakeLine("BLOWING the dick daily.", 29f));
+                            subtitles.Add(MakeLine("Your life is nothing, you serve ZERO purpose!", 32f));
+                            subtitles.Add(MakeLine("Come forth, Child of Man", 34.69f));
+                            subtitles.Add(MakeLine("and kill yourself", 36.73f));
+                            subtitles.Add(MakeLine("NOW!", 38.036f));
                         }
                         //update subtitles if needed
                         if (replaced)
